e7ad0d2b69000ece6261e58bd75e9fead80df582
Yashira-De-Silva <yashiradesilva@gmail.com>
2024-09-12 13:28:00 +0530

    edit

--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,12 @@
-print("HI")
-print("Hello")
+#print("HI")
+#print("Hello")
 people = 20
 cats = 30
-if(people < cats):
-    print("Too many cats! The world is doomed!")
+#if(people < cats):
+    #print("Too many cats! The world is doomed!")
+#print(2==1)
+#print('string'== 'string') 
+say_hello = "Hello!"
+say_hello =[3]
+language_name = "Python"
+language_name*3
